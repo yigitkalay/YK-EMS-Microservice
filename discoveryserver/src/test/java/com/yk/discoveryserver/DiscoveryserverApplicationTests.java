e53d707bd9924c6f04b72f8e026ad90b8fff246c
Yiğit <ygtkly06@gmail.com>
2024-05-07 00:40:45 +0300

    Add Spring Cloud & Netflix Eureka

---
+++ discoveryserver/src/test/java/com/yk/discoveryserver/DiscoveryserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yk.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- employeeservice/src/main/java/com/yk/employeeservice/EmployeeserviceApplication.java
+++ employeeservice/src/main/java/com/yk/employeeservice/EmployeeserviceApplication.java
@@ -2,6 +2,7 @@ package com.yk.employeeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 public class EmployeeserviceApplication {
