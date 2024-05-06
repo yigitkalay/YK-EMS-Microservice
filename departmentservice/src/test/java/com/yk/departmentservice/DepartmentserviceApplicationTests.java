e99e0085bab35ae9b7a15df945c957f8b14f3fee
Yiğit <ygtkly06@gmail.com>
2024-05-06 17:11:23 +0300

    Create Project

---
+++ departmentservice/src/test/java/com/yk/departmentservice/DepartmentserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yk.departmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
